fc9defc51115d4edd3a83ec7234c018d62781417
valeriva <valentinoaustral@gmail.com>
2022-10-14 12:48:04 -0300

    14/10 cambios respecto a la pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,31 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tablinks/tablinks.module').then( m => m.TablinksPageModule)
-  }
+  },
+  {
+    path: 'Trabajo',
+    loadChildren: () => import('./public/pages/trabajo/trabajo.module').then( m => m.TrabajoPageModule)
+  },
+  {
+    path: 'Eucacion',
+    loadChildren: () => import('./public/pages/educacion/educacion.module').then( m => m.EducacionPageModule)
+  },
+  {
+    path: 'Cultura',
+    loadChildren: () => import('./public/pages/cultura/cultura.module').then( m => m.CulturaPageModule)
+  },
+  {
+    path: 'Salud',
+    loadChildren: () => import('./public/pages/salud/salud.module').then( m => m.SaludPageModule)
+  },
+  {
+    path: 'Transporte',
+    loadChildren: () => import('./public/pages/transporte/transporte.module').then( m => m.TransportePageModule)
+  },
+  {
+    path: 'Deportes',
+    loadChildren: () => import('./public/pages/deportes/deportes.module').then( m => m.DeportesPageModule)
+  },
 ];
 
 @NgModule({
